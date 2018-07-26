2b99f35553ed5d615b359934bd5f424f521a349d
indianajim <jimrunkel@gmail.com>
2018-07-25 21:23:16 -0400

    finished great step-by-step walkthrough

---
+++ MVCdemo/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using MVCdemo.Models;
+using System.Web.Mvc;
+
+namespace MVCdemo.Controllers
+{
+    //handles user interaction logic
+    public class TestController : Controller
+    {
+        public string MyFirstMethod()
+        {
+            return "Welcome to my method!";
+        }
+
+        public ActionResult MyFirstViewMethod()
+        {
+
+            CustomerBusinessAccessLayer b = new CustomerBusinessAccessLayer();
+            Customer c = b.GetCustomer();
+
+            ViewBag.CustomerData = c;
+            return View("MyFirstView");
+        }
+
+    }
+}
---
+++ MVCdemo/Models/CustomerBusinessAccessLayer.cs
@@ -0,0 +1,14 @@
+﻿namespace MVCdemo.Models
+{
+    public class CustomerBusinessAccessLayer
+    {
+        public Customer GetCustomer()
+        {
+            Customer c = new Customer();
+            c.CustomerName = "Some Customer";
+            c.Address = "SomeAddress";
+            c.Age = 55;
+            return c;
+        }
+    }
+}
